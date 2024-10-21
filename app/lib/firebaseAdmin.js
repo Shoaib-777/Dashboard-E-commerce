cabe36483f3fc88259b38d12f9b4a44d2251ef15
Shoaib-777 <shaikmohammedshoaib777@gmail.com>
2024-10-21 16:24:40 +0530

    Updated

--- app/lib/firebaseAdmin.js
+++ app/lib/firebaseAdmin.js
@@ -7,7 +7,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
   });
 }
 
--- app/lib/firebaselib.js
+++ app/lib/firebaselib.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; // Import Firebase Storage
 
 const firebaseConfig = {
-    apiKey: process.env.Api_Key,
-    authDomain: process.env.AuthDomain,
-    projectId: process.env.Project_Id ,
-    storageBucket:  process.env.Storage_BUcket,
-    messagingSenderId:  process.env.Messaging_Sender_ID,
-    appId:  process.env.APP_Id
+    apiKey: process.env.NEXT_PUBLIC_Api_Key,
+    authDomain: process.env.NEXT_PUBLIC_AuthDomain,
+    projectId: process.env.NEXT_PUBLIC_Project_Id ,
+    storageBucket:  process.env.NEXT_PUBLIC_Storage_BUcket,
+    messagingSenderId:  process.env.NEXT_PUBLIC_Messaging_Sender_ID,
+    appId:  process.env.NEXT_PUBLIC_APP_Id
 };
 
 const app = initializeApp(firebaseConfig);
