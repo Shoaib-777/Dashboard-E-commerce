b006eb2d1a9234517adacb1e4e330637129e95ba
Shoaib-777 <shaikmohammedshoaib777@gmail.com>
2024-10-21 16:26:33 +0530

    Next Updated

--- app/lib/firebaseAdmin.js
+++ app/lib/firebaseAdmin.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 
 // Parse the service account key from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY);
 
 // Check if Firebase Admin has been initialized
 if (!admin.apps.length) {
